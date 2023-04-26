4bab23a8aa6f7b57f4744d2ca51b08343fb38a15
yoncaaa <yoncaakceylan@web.de>
2023-04-26 20:14:56 +0200

    a

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log('testing');
+
+let apiQuotes = [];
+
+//show new quote
+function newQuote(){
+    //pick a random quote from api quotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+
+}
+
+// get quotes from API
+async function getQuotes(){ //async function heißt dass es das laden der seite nicht beeinflusst
+    const apiUrl = 'https://type.fit/api/quotes';
+    try{
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error){
+        //catch error here
+    }
+}
+
+
+//on load
+getQuotes();
